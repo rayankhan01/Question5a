33023546edbc9f667efdfbde16a42b3c467e0803
rayankhan01 <rk12172002@gmail.com>
2024-03-09 19:43:28 +0500

    Stripping Names

---
+++ main.js
@@ -0,0 +1,3 @@
+var myname = "\t\n Rayan \t\n"; //
+console.log(myname); //
+console.log(myname.trim()); //
---
+++ main.ts
@@ -0,0 +1,3 @@
+let myname: string = "\t\n Rayan \t\n"; //
+console.log(myname); //
+console.log(myname.trim()); //
